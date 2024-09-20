86b6e80fa7cb861e1320274d257fbdd2bf4a83e8
Joao Machado <machado000@gmail.com>
2024-09-20 13:12:07 +0000

    Added docstrings, refactor random_coordinates() method

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/home/joaom/Projects/tdnews-robot/.venv/bin/python
+#!/home/joaom/Projects/pagevisit-robot/.venv/bin/python
 
 import asyncio
 import os
@@ -11,7 +11,8 @@ from time import sleep
 
 def main():
     '''
-    Main function to execute the job.
+    Main function to run a asynchronous job, visiting a specified page repeatedly
+    using multiple browser instances to mimic user behavior.
     '''
     page_to_visit = 'https://tododianews.com.br'
     cpu_cores = os.cpu_count() or 1  # Get the number of CPU cores
@@ -31,18 +32,27 @@ def main():
 
 async def visit_page(playwright, page_to_visit) -> None:
     '''
-    Visit a page with a rotated header and take a screenshot
+    Visit a page with a rotated device/header.
+
+    Args:
+        playwright: The playwright instance to launch the browser.
+        page_to_visit: The URL of the page to visit.
     '''
     # Select a random device from Playwright devices
     device_name = random.choice(list(playwright.devices.keys()))
     device = playwright.devices[device_name]  # Get the device configuration
 
+    # Get proxy settings from environment variables
+    proxy_server = 'http://p.webshare.io:80'
+    proxy_username = os.getenv('PROXY_USERNAME')  # Proxy username from .env
+    proxy_password = os.getenv('PROXY_PASSWORD')  # Proxy password from .env
+
     browser = await playwright.chromium.launch(
         headless=True,
         proxy={
-            'server': 'http://p.webshare.io:80',
-            'username': 'holoscompany-rotate',
-            'password': 'tgbsphkipr6q'
+            'server': proxy_server,
+            'username': proxy_username,
+            'password': proxy_password
         },
     )
 
@@ -70,32 +80,26 @@ async def visit_page(playwright, page_to_visit) -> None:
         await page.evaluate(f'window.scrollTo(0, {i});')
         await page.wait_for_timeout(100)  # Adjust the wait time for slower/faster scroll
 
-    # Filter links that contain 'https://tododianews.com.br/noticia/'
+    # Filter links that contain 'https://tododianews.com.br/noticia/', adjust as needed
     filtered_links = await page.locator('a[href*="https://tododianews.com.br/noticia/"]').element_handles()
-
-    for i in range(0, scroll_height, 100):
-        await page.evaluate(f'window.scrollTo(0, {i});')
-        await page.wait_for_timeout(100)  # Adjust the wait time for slower/faster scroll
 
     if filtered_links:
         random_link = random.choice(filtered_links)
         href_value = await random_link.get_attribute('href')
-        print(f'{proxy_ip} - {device_name} - {href_value.replace('https://tododianews.com.br', '')}')
+        print(f'{proxy_ip} - {device_name} - {href_value.replace("https://tododianews.com.br", "")}')
 
         try:
-            await random_link.click(timeout=30000)  # Click the random link
+            await random_link.click(timeout=30000)  # Click the random link on filtered list
             await page.wait_for_load_state('load', timeout=30000)  # Wait for the page to fully load
         except TimeoutError:
             print('Element not visible or clickable.')
-
     else:
         print('No matching links found.')
 
-    # # Save the screenshot
+    # Save the screenshot (if needed)
     # os.makedirs('screenshots', exist_ok=True)
     # timestamp = pendulum.now().format('YYYYMMDD-HHmmss-SSSS')
     # filepath = os.path.join('screenshots', f'{timestamp}.png')
-
     # await page.screenshot(path=filepath)
     # print(f'Saved screenshot as {filepath}')
 
@@ -107,20 +111,23 @@ async def visit_page(playwright, page_to_visit) -> None:
 
 async def job(page_to_visit: str, simultaneous_runs: int) -> None:
     '''
-    Run multiple page visits concurrently.
+    Run multiple visit_page() methods concurrently.
+
+    Args:
+        page_to_visit: The URL of the page to visit.
+        simultaneous_runs: The number of concurrent page visits.
     '''
     async with async_playwright() as playwright:
-        tasks = []
-        for _ in range(simultaneous_runs):
-            # headers = random.choice(header_list)
-            tasks.append(visit_page(playwright, page_to_visit))
-
+        tasks = [visit_page(playwright, page_to_visit) for _ in range(simultaneous_runs)]
         await asyncio.gather(*tasks)
 
     return None
 
 
 async def save_device_list_to_file() -> None:
+    '''
+    Save the list of available devices to a text file for reference.
+    '''
     async with async_playwright() as playwright:
         all_devices = playwright.devices
 
@@ -131,19 +138,32 @@ async def save_device_list_to_file() -> None:
     return None
 
 
-def random_coordinates(radius_km=50) -> dict:
-    # Brasília coordinates
-    latitude_brasilia = -15.8267
-    longitude_brasilia = -47.9218
+def random_coordinates(latitude_origin: float = None, longitude_origin: float = None, radius_km=50) -> dict:
+    '''
+    Generate random geographic coordinates within a specified radius.
 
-    # Convert km to degrees (1 degree ~ 111 km)
+    Args:
+        latitude_origin (float): The origin latitude; defaults to Brasília's latitude if not provided.
+        longitude_origin (float): The origin longitude; defaults to Brasília's longitude if not provided.
+        radius_km (int): The radius in kilometers to generate random coordinates.
+
+    Returns:
+        dict: A dictionary with random longitude and latitude.
+    '''
+    # Default coordinates for Brasília, Brazil, if not provided
+    latitude_origin = latitude_origin or -15.8267
+    longitude_origin = longitude_origin or -47.9218
+
+    # Convert kilometers to degrees (approximately 1 degree ~ 111 km)
     radius_in_degrees = radius_km / 111
 
+    # Generate random offsets within the specified radius
     lat_offset = random.uniform(-radius_in_degrees, radius_in_degrees)
     lon_offset = random.uniform(-radius_in_degrees, radius_in_degrees)
 
-    latitude = latitude_brasilia + lat_offset
-    longitude = longitude_brasilia + lon_offset
+    # Calculate the new random latitude and longitude
+    latitude = latitude_origin + lat_offset
+    longitude = longitude_origin + lon_offset
 
     return {"longitude": longitude, "latitude": latitude}
 
